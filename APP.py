662dd67cb9c63109173bb58898696c54ac3b7959
shivaprasadpattanshetty16-pixel <shivaprasadpattanshetty16@gmail.com>
2026-04-17 10:48:51 +0530

    Replace image in APP.py with AI.jpeg

--- APP.py
+++ APP.py
@@ -17,7 +17,7 @@ os.environ["USER_AGENT"] = "rag-app"
 load_dotenv()
 groq_api_key = st.secrets["GROQ_API_KEY"]
 st.set_page_config(page_title="Dynamic RAG with Groq", layout="wide")
-st.image("PragyanAI_Transperent.png")
+st.image("AI.jpeg")
 st.title("Dynamic RAG with Groq, FAISS, and Llama3")
 if "vector" not in st.session_state:
     st.session_state.vector = None
